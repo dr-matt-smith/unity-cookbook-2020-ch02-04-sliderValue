7b98556c28b1a3870f7a9389ae7b88bcaf1bee81
ShaunFerns <shaun.ferns@tudublin.ie>
2020-02-11 11:43:18 +0000

    1st commit

---
+++ Assets/_Scripts/SliderValueToText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValueToText : MonoBehaviour
+{
+    public Slider sliderUI;
+    private Text textSliderValue;
+
+    void Awake()
+    {
+        textSliderValue = GetComponent<Text>();
+    }
+
+    void Start()
+    {
+        ShowSliderValue();
+    }
+
+    public void ShowSliderValue()
+    {
+        string sliderMessage = "Slider value = " + sliderUI.value;
+        textSliderValue.text = sliderMessage;
+    }
+}
